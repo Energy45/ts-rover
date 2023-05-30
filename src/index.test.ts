0b8b94ff9ed3ded9de50bddfd3d2a8bdfc8fe0f6
Dylan ANTONIOTTI <dylan.antoniotti@gmail.com>
2023-05-30 15:46:52 +0200

    feat(tests): for turnRight and left

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,7 +4,7 @@ import { Direction } from "./enums/direction.enum";
 import { Point } from "./types/point.type";
 
 describe('turnLeft function', () => {
-    test('it should change direction to East', () => {
+    test('it should change direction to North', () => {
         const map = new MarsMap(50, 50);
         const basePosition: Point = {
             x: 0,
@@ -15,5 +15,96 @@ describe('turnLeft function', () => {
         const positionBis = rover.turnLeft()
         console.log(positionBis.direction)
         expect(positionBis.direction).toEqual(Direction.North)
+    })
+
+    test('it should change direction to West', () => {
+        const map = new MarsMap(50, 50);
+        const basePosition: Point = {
+            x: 0,
+            y: 0,
+            direction: Direction.North,
+          }
+        const rover = new Rover(map, basePosition)
+        const positionBis = rover.turnLeft()
+        console.log(positionBis.direction)
+        expect(positionBis.direction).toEqual(Direction.West)
+    })
+
+    test('it should change direction to South', () => {
+        const map = new MarsMap(50, 50);
+        const basePosition: Point = {
+            x: 0,
+            y: 0,
+            direction: Direction.West,
+          }
+        const rover = new Rover(map, basePosition)
+        const positionBis = rover.turnLeft()
+        console.log(positionBis.direction)
+        expect(positionBis.direction).toEqual(Direction.South)
+    })
+    test('it should change direction to East', () => {
+        const map = new MarsMap(50, 50);
+        const basePosition: Point = {
+            x: 0,
+            y: 0,
+            direction: Direction.South,
+          }
+        const rover = new Rover(map, basePosition)
+        const positionBis = rover.turnLeft()
+        console.log(positionBis.direction)
+        expect(positionBis.direction).toEqual(Direction.East)
+    })
+})
+
+describe('turnRight function', () => {
+    test('it should change direction to North', () => {
+        const map = new MarsMap(50, 50);
+        const basePosition: Point = {
+            x: 0,
+            y: 0,
+            direction: Direction.West,
+          }
+        const rover = new Rover(map, basePosition)
+        const positionBis = rover.turnRight()
+        console.log(positionBis.direction)
+        expect(positionBis.direction).toEqual(Direction.North)
+    })
+
+    test('it should change direction to West', () => {
+        const map = new MarsMap(50, 50);
+        const basePosition: Point = {
+            x: 0,
+            y: 0,
+            direction: Direction.South,
+          }
+        const rover = new Rover(map, basePosition)
+        const positionBis = rover.turnRight()
+        console.log(positionBis.direction)
+        expect(positionBis.direction).toEqual(Direction.West)
+    })
+
+    test('it should change direction to South', () => {
+        const map = new MarsMap(50, 50);
+        const basePosition: Point = {
+            x: 0,
+            y: 0,
+            direction: Direction.East,
+          }
+        const rover = new Rover(map, basePosition)
+        const positionBis = rover.turnRight()
+        console.log(positionBis.direction)
+        expect(positionBis.direction).toEqual(Direction.South)
+    })
+    test('it should change direction to East', () => {
+        const map = new MarsMap(50, 50);
+        const basePosition: Point = {
+            x: 0,
+            y: 0,
+            direction: Direction.North,
+          }
+        const rover = new Rover(map, basePosition)
+        const positionBis = rover.turnRight()
+        console.log(positionBis.direction)
+        expect(positionBis.direction).toEqual(Direction.East)
     })
 })
